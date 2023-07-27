90d5e6a42155869e65878dcb83d44dd7f70b1e86
alanmarietta <alandmarietta@gmail.com>
2023-07-27 12:51:41 -0400

    Updated files

---
+++ gpttrading.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <string>
+#include <map>
+#include <cstdio> // For popen and pclose
+#include <curl/curl.h>
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *userp) {
+    size_t totalSize = size * nmemb;
+    userp->append((char *)contents, totalSize);
+    return totalSize;
+}
+
+int main() {
+    std::map<std::string, double> lastTradePrices;
+    std::vector<std::string> symbols = {
+        "AAPL", "MSFT", "AMZN", "FB", "GOOGL",
+        "GOOG", "TSLA", "NVDA", "BRK.B", "JPM",
+        "JNJ", "V", "PG", "HD", "UNH",
+        "MA", "DIS", "BAC", "VZ", "XOM"
+    };
+
+    CURL *curl = curl_easy_init();
+
+    if (curl) {
+        for (const std::string &symbol : symbols) {
+            std::string responseString;
+            std::string url = "https://data.alpaca.markets/v1/last/stocks/" + symbol;
+            curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+            struct curl_slist *headers = NULL;
+            headers = curl_slist_append(headers, "APCA-API-KEY-ID: PKO3RZBLQ0QU5NSTGBDY");
+            headers = curl_slist_append(headers, "APCA-API-SECRET-KEY: YpvjSz8Z34IEpdu8MZ1BdGUAPDOfqan7VmKmQqyV");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &responseString);
+
+            CURLcode res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            } else {
+                json jsonResponse = json::parse(responseString);
+                std::cout << "Symbol: " << jsonResponse["last"]["symbol"] << std::endl;
+                double latestPrice = jsonResponse["last"]["price"];
+                std::cout << "Latest price: " << latestPrice << std::endl;
+
+                if (lastTradePrices.find(symbol) != lastTradePrices.end()) {
+                    double lastTradePrice = lastTradePrices[symbol];
+                    double percentageChange = 100.0 * (latestPrice - lastTradePrice) / lastTradePrice;
+
+                    std::string command = "python openai_advisor.py " + std::to_string(percentageChange) + " " + std::to_string(latestPrice);
+                    FILE* pipe = popen(command.c_str(), "r");
+                    if (!pipe) throw std::runtime_error("popen() failed!");
+                    char buffer[128];
+                    std::string result = "";
+                    while (!feof(pipe)) {
+                        if (fgets(buffer, 128, pipe) != NULL)
+                            result += buffer;
+                    }
+                    pclose(pipe);
+
+                    std::cout << "GPT-3's recommendation for " << symbol << ": " << result << std::endl;
+                }
+                lastTradePrices[symbol] = latestPrice;
+            }
+            curl_slist_free_all(headers);
+        }
+        curl_easy_cleanup(curl);
+    }
+
+    return 0;
+}
+
---
+++ openai_advisor.py
@@ -0,0 +1,32 @@
+import openai
+import sys
+
+openai.api_key = "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"
+
+def get_advice(percentage_change, current_price):
+    # Prepare the input for GPT-3 based on the received data
+    prompt = f"""The current price is {current_price}. 
+It has changed by {percentage_change}% since the last trade. 
+According to a simple trading strategy, if the price has increased by more than 5% since the last trade, it should be sold.
+If it has decreased by more than 5%, it should be bought.
+Otherwise, the position should be held.
+Based on this information and strategy, should the stock be bought, sold, or held?"""
+
+    # Make a completion call to OpenAI GPT-3
+    response = openai.Completion.create(engine="davinci", prompt=prompt, max_tokens=50)
+
+    # Return the generated advice
+    return response.choices[0].text.strip()
+
+
+if __name__ == "__main__":
+    # Read input data from command line arguments
+    percentage_change = float(sys.argv[1])
+    current_price = float(sys.argv[2])
+    
+    # Get the advice based on the input data
+    advice = get_advice(percentage_change, current_price)
+
+    # Print the advice to the console
+    print(advice)
+
